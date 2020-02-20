1db135e5cab290ab9fe173fba4e6a530ac061af6
AlecVidamo <55145798+AlecVidamo@users.noreply.github.com>
2020-02-19 20:19:18 -0500

    Created Drag and draw script that creates a line when the player hold down the mouse button. Need to edit as lines are not colliding with objects

---
+++ Assets/Scripts/ClickAndDraw.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAndDraw : MonoBehaviour
+{
+    public GameObject linePrefab;
+    public GameObject currentLine;
+    public LineRenderer lineRenderer;
+    public Rigidbody rigidBody;
+    public List<Vector2> mousePos;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) 
+        {
+            CreateLine();
+        }
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 tempMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if(Vector2.Distance(tempMousePos, mousePos[mousePos.Count - 1]) > .1f)
+            {
+                UpdateLine(tempMousePos);
+            }
+        }
+    }
+
+    void CreateLine()
+    {
+        currentLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+        lineRenderer = currentLine.GetComponent<LineRenderer>();
+        rigidBody = currentLine.GetComponent<Rigidbody>();
+        mousePos.Clear();
+        mousePos.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        mousePos.Add(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        lineRenderer.SetPosition(0, mousePos[0]);
+        lineRenderer.SetPosition(1, mousePos[1]);
+    }
+
+    void UpdateLine(Vector2 newMousePos)
+    {
+        mousePos.Add(newMousePos);
+        lineRenderer.positionCount++;
+        lineRenderer.SetPosition(lineRenderer.positionCount - 1, newMousePos);
+    }
+}
